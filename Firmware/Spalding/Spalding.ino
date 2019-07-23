9728f5e56a9b28289b8c58972bd99ff7d41084db
FischerMoseley <fischerm@mit.edu>
2019-07-23 11:39:26 -0600

    Added accelerometer integration

--- Firmware/Spalding/Spalding.ino
+++ Firmware/Spalding/Spalding.ino
@@ -30,15 +30,14 @@ motor from the z-axis. You choose this angle, but the microcontroller doesn't ca
 
 /*--------------- State Variables -----------------*/
 
-float gyroPosition[3]; //literally have no idea how to get an euler output, just quaternions at the moment
-float gyroRate[3]; //in the form x-axis rad/sec, y-axis, rad/sec z-axis rad/sec
-uint16_t stateVector[3]; //in the form x-translation, y-translation, z-rotation
-uint16_t stepperSpeeds[3]; //in the form motor1-rpm, motor2-rpm, motor3-rpm
+double gyroPosition[3]; //literally have no idea how to get an euler output, just quaternions at the moment
+double gyroRate[3]; //in the form x-axis rad/sec, y-axis rad/sec, z-axis rad/sec
+double stateVector[3]; //in the form x-translation, y-translation, z-rotation
 
 /*------------- Hardware Configuration ----------- */
 //Stepper motor configuration
-#define StepsPerRotation 200
-#define StepperMaxSpeed 100
+#define STEPS_PER_REV 200
+#define STEPPER_MAX_SPEED 1000
 AccelStepper stepper1(1, 3, 2);
 AccelStepper stepper2(1, 5, 4);
 AccelStepper stepper3(1, 6, 7);
@@ -47,8 +46,8 @@ AccelStepper stepper3(1, 6, 7);
 BNO080 IMU;
 
 //PID configuration
-double xInput, xOutput, xSetPoint;
-double yInput, yOutput, ySetPoint;
+double xSetpoint = 0;
+double ySetpoint = 0;
 
 //define tuning parameters
 double kP = 100;
@@ -56,18 +55,26 @@ double kI = 0;
 double kD = 0;
 
 //Specify links and initial tuning parameters
-PID xAxisPID(&xInput, &xOutput, &xSetpoint, kP, kI, kD);
-PID yAxisPID(&yInput, &yOutput, &ySetpoint, kp, kI, kD);
+PID xAxisPID(&gyroRate[0], &stateVector[0], &xSetpoint, kP, kI, kD, DIRECT);
+PID yAxisPID(&gyroRate[1], &stateVector[1], &ySetpoint, kP, kI, kD, DIRECT);
 
-void updateGyroStateVariables(float* gyroRate){
+
+/*------------- Wrapper Functions ----------- */
+
+void updateGyroStateVariables(double gyroRate[]){
   if(IMU.dataAvailable()){
-    gyroRate[0] = IMU.getGyroX();
-    gyroRate[1] = IMU.getGyroY();
-    gyroRate[2] = IMU.getGyroZ();
+    gyroRate[0] = IMU.getAccelX();
+    gyroRate[1] = IMU.getAccelY();
+    gyroRate[2] = IMU.getAccelZ();
   }
 }
 
-void updateStepperSpeeds(float* stateVector, float* stepperSpeeds){
+void updateStepperSpeeds(double stateVector[]){
+  double stepperSpeeds[3];
+  double x = stateVector[0];
+  double y = stateVector[1];
+  double w_z = stateVector[2];
+
   stepperSpeeds[0] = (-2*x*csc_theta - w_z*ball_radius*sec_theta)/(3*wheel_radius);
   stepperSpeeds[1] = ((x + sqrt3*y)*csc_theta - w_z*ball_radius*sec_theta)/(3*wheel_radius);
   stepperSpeeds[2] = ((x - sqrt3*y)*csc_theta - w_z*ball_radius*sec_theta)/(3*wheel_radius);
@@ -81,19 +88,46 @@ void updateStepperSpeeds(float* stateVector, float* stepperSpeeds){
   stepper3.runSpeed();
 }
 
+void printStateVector(double stateVector[]){
+  Serial.print("Gyro StateVector: X: ");
+  Serial.print(stateVector[0]);
+  Serial.print("  Y: ");
+  Serial.print(stateVector[1]);
+  Serial.print("  ω_Z: ");
+  Serial.print(stateVector[2]);
+  Serial.println();
+}
+
 void setup() {
+  //start up all them interfaces
   Serial.begin(115200);
   Wire.begin();
   Wire.setClock(400000);
   IMU.begin();
-  myIMU.enableGyro(20); //Send data update every 20ms
+  IMU.enableAccelerometer(20); //Send data update every 20ms
 
-  stepper1.setMaxSpeed(1000);	
-  stepper2.setMaxSpeed(1000);	
-  stepper3.setMaxSpeed(1000);	
+  //set all the max motor speeds
+  stepper1.setMaxSpeed(STEPPER_MAX_SPEED);	
+  stepper2.setMaxSpeed(STEPPER_MAX_SPEED);	
+  stepper3.setMaxSpeed(STEPPER_MAX_SPEED);
+
+  //turn on PID
+  xAxisPID.SetOutputLimits(-1000, 1000);
+  yAxisPID.SetOutputLimits(-1000, 1000);
+  xAxisPID.SetMode(AUTOMATIC);
+  yAxisPID.SetMode(AUTOMATIC);
 }
 
 void loop() {
-  updateGyroStateVariables(&gyroRate);
-  updateStepperSpeeds(&stateVector, &stepperSpeeds);
+  //get data
+  updateGyroStateVariables(gyroRate);
+
+  //recompute PID axis
+  xAxisPID.Compute();
+  yAxisPID.Compute();
+
+  printStateVector(stateVector);
+
+  //update stepper speeds 
+  updateStepperSpeeds(stateVector);
 }
